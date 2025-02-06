4c9a60e3bfc32aabc297387d0e962f5fe3816da2
rzounlom <rzounlom@gmail.com>
2025-02-06 07:36:41 -0500

    remove navbar

--- components/overview/Overview.tsx
+++ components/overview/Overview.tsx
@@ -1,12 +1,7 @@
-import Navbar from "../navbar/Navbar";
-import { createNewUser } from "@/utils/actions/auth";
-
 export default async function Overview() {
-  await createNewUser();
   return (
     <div>
       <h1>Overview</h1>
-      <Navbar />
     </div>
   );
 }
