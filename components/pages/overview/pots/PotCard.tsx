03387ff2bb958bbf04f67c5e3aecca15c312a260
rzounlom <rzounlom@gmail.com>
2025-02-15 13:40:33 -0500

    minor changes

--- components/pages/overview/pots/PotCard.tsx
+++ components/pages/overview/pots/PotCard.tsx
@@ -1,19 +1,20 @@
 import { FC } from "react";
 import { Pot } from "@/lib/types";
+import clsx from "clsx";
 
 type PotCardProps = {
   pot: Pot;
 };
 
 const PotCard: FC<PotCardProps> = ({ pot }) => {
+  const cardStyles = clsx(`block w-1 h-full rounded-full ${pot?.color} mr-4`);
+
   return (
     <div className="flex items-center h-[55px] md:h-[43px]">
-      <span
-        className={`block w-1 h-full rounded-full ${pot.color} mr-4`}
-      ></span>
+      <span className={cardStyles}></span>
       <div>
-        <p className="text-sm text-gray-500">{pot.name}</p>
-        <p className="font-bold mt-2">${pot.amount}</p>
+        <p className="text-sm text-gray-500">{pot?.name}</p>
+        <p className="font-bold mt-2">${pot?.amount}</p>
       </div>
     </div>
   );
